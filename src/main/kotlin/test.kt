61bf8972c66762673a3c34549c494a51b153cbeb
Provakor Das <fakibuzzkonik@gmail.com>
2021-05-24 01:04:13 +0600

    day two practice

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -3,12 +3,22 @@ import java.util.*
 import kotlin.math.max
 import kotlin.math.min
 
-fun main(args :Array<String>){
-    val input=Scanner(System.`in`)
-    val n1=input.nextInt()
-    val n2=input.nextInt()
+fun main(args :Array<String>) {
+    val input = Scanner(System.`in`)
+    val n1 = input.nextInt()
+    val n2 = input.nextInt()
+    var i :Int = 1;
+    var ch :Int = 0
+    for(i in i ..n1){
+        val num = input.nextInt()
+        if(num > 0 && num > n2){
+            ch++
+        }
+    }
+    println(ch)
 
-    val set1 = mutableSetOf<Int>()
+}
+    /*val set1 = mutableSetOf<Int>()
     val set2 = mutableSetOf<Int>()
     (1..n1).forEach { set1.add(input.nextInt()) }
     (1..n2).forEach { set2.add(input.nextInt()) }
@@ -17,4 +27,4 @@ fun main(args :Array<String>){
     } else {
         println(min(set1.min()!!, set2.min()!!)*10+ max(set1.min()!!, set2.min()!!))
     }
-}
+}*/
